8c556d7322df08101a957b9dd865e08a5517abe8
SeanLogsdon22 <101378568+SeanLogsdon22@users.noreply.github.com>
2022-07-12 11:22:46 -0700

    Add files via upload

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import bundle from '../bundler'
+import CodeEditor from './code-editor'
+import Preview from './preview'
+import Resizable from './resizable'
+
+const CodeCell = () => {
+    const [code, setCode] = useState('')
+    const [err, setErr]= useState('')
+    const [input, setInput] = useState('')
+
+    // debouncing for bundling 
+    useEffect(() => {
+        const timer = setTimeout(async () => {
+            const output = await bundle(input);
+            setCode(output.code)
+            setErr(output.err)
+        }, 750)
+
+        return () => {
+            clearTimeout(timer)
+        }
+
+    }, [input])
+
+    return (
+        <Resizable direction="vertical">
+        <div style={{ height: '100%', display: 'flex', flexDirection: 'row'}}>
+            <Resizable direction="horizontal">
+                <CodeEditor 
+                    initialValue='const a = 1'
+                    onChange={(value) => setInput(value)}
+                />
+            </Resizable>
+            <Preview code={code} err={err}/>
+        </div>
+        </Resizable>
+    )
+}
+
+export default CodeCell
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import ReactDOM from 'react-dom'
+import TextEditor from './components/text-editor'
+
+const App = () => {
+    return (
+        <>
+            <TextEditor />
+        </>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
